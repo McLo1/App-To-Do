9f762f8cc23691a930511b0e1ba47d50311023f4
McLo1 <marcelo.conceicao1718@gmail.com>
2025-08-19 23:29:45 -0300

    Adicionado icones ao tab-navigator

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomePageScreen from './src/pages/HomeScreen/index.js';
 import AboutScreen from './src/pages/AboutScreen/index.js';
 import SettingScreen from './src/pages/SettingsScreen/index.js';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -11,10 +13,27 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomePageScreen}/>
-        <Tab.Screen name='About' component={AboutScreen}/>
-        <Tab.Screen name='Settings' component={SettingScreen}/>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home-outline';
+            } else if (route.name === 'About') {
+              iconName = 'information-circle-outline';
+            }
+            else if(route.name === 'Settings') {
+              iconName = 'settings-outline'
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name='Home' component={HomePageScreen} />
+        <Tab.Screen name='About' component={AboutScreen} />
+        <Tab.Screen name='Settings' component={SettingScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,6 +1,6 @@
 import { SafeAreaView, } from "react-native-safe-area-context";
 import { Text, TouchableOpacity } from "react-native";
-import {Styles} from './style'
+import { Styles } from './style'
 
 function mostrarMensagem() {
     alert('Receita Cadastrada')
@@ -12,8 +12,10 @@ export default function HomePageScreen() {
 
         <SafeAreaView style={Styles.Container}>
             <Text style={Styles.Tittle}>Tela Inicial</Text>
+            <Text>Adicionar uma Receita</Text>
 
-            <TouchableOpacity style={Styles.Button} onPress={mostrarMensagem}> 
+            
+            <TouchableOpacity style={Styles.Button} onPress={mostrarMensagem}>
 
                 <Text style={Styles.ButtonText}>Toque aqui</Text>
 
